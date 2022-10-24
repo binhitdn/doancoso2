858755678d5d1ae6a58fbf8c63d84ea98e95b8a2
Bình Phan <binhpc.21it@vku.udn.vn>
2022-10-24 21:48:15 +0700

    commit

--- src/containers/Guest/ContainerPage/SideBar.js
+++ src/containers/Guest/ContainerPage/SideBar.js
@@ -40,14 +40,14 @@ const SideBar = () => {
             link: "/doctor",
             icon: "fa-solid fa-user-doctor"
         },
-        {
-            id: 4,
-            en: "Health Facilities",
-            vi: "Cơ sở y tế",
-            jp: "医療施設",
-            link: "/clinic",
-            icon: "fa-solid fa-house-chimney-medical"
-        },
+        // {
+        //     id: 4,
+        //     en: "Health Facilities",
+        //     vi: "Cơ sở y tế",
+        //     jp: "医療施設",
+        //     link: "/clinic",
+        //     icon: "fa-solid fa-house-chimney-medical"
+        // },
         {
             id: 5,
             en: "HandBook",
@@ -56,14 +56,14 @@ const SideBar = () => {
             link: "/handbook",
             icon: "fa-solid fa-newspaper"
         },
-        {
-            id: 6,
-            en: "Community",
-            vi: "Cộng đồng",
-            jp: "コミュニティ",
-            link: "/community",
-            icon: "fa-solid fa-earth-americas"
-        }
+        // {
+        //     id: 6,
+        //     en: "Community",
+        //     vi: "Cộng đồng",
+        //     jp: "コミュニティ",
+        //     link: "/community",
+        //     icon: "fa-solid fa-earth-americas"
+        // }
     ]
 
     return (
